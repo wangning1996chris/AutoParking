d4c73ad9b17da9db4bfe0d9568380136da3fa793
zhangyu <8611452@qq.com>
2023-03-02 09:59:44 +0800

    add collider for goal

--- Examples/MAPD/Script/Parking/PitCarAgent.cs
+++ Examples/MAPD/Script/Parking/PitCarAgent.cs
@@ -26,8 +26,10 @@ public class PitCarAgent : Agent
     private float t_distance;
     private float MaxDistance;
     private float p_distance;
+    private float t_dist, p_dist;
+
     private float t_y_pos; //terrain y 
-    private const int RayNum = 18;
+    private const int RayNum = 36;
 
     public override void Initialize()
     {
@@ -63,7 +65,8 @@ public class PitCarAgent : Agent
         // reset distance
         MaxDistance = (m_Car.transform.position - Destination).magnitude;
         t_distance = (m_Car.transform.position - Destination).magnitude;
-       
+        t_dist = (m_Car.transform.position - Destination).magnitude;
+        p_dist = (m_Car.transform.position - Destination).magnitude;
 
         // reset p_value
         p_distance = t_distance;
@@ -87,11 +90,16 @@ public class PitCarAgent : Agent
         t_distance = (m_Car.transform.position - Destination).magnitude;
         
         float distance_reward = -t_distance / MaxDistance;
-        // float distance_reward = (p_distance - t_distance) / MaxDistance * 100;
+        // // float distance_reward = (p_distance - t_distance) / MaxDistance * 100;
         
         AddReward(distance_reward * 2);
-        Debug.Log(distance_reward * 2);
+        // Debug.Log(distance_reward * 2);
+
+        t_dist = (m_Car.transform.position - Destination).magnitude;
+        AddReward((p_dist - t_dist) * 100+distance_reward * 2);
+        Debug.Log((p_dist - t_dist) * 100+distance_reward * 2);
         p_distance = t_distance;
+        p_dist = t_dist;
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
@@ -128,7 +136,7 @@ public class PitCarAgent : Agent
         // Success
         Vector3 CarRota = m_Car.transform.rotation.eulerAngles;
         float speed = Math.Abs(m_Car.ForwardSpeed);
-        if (t_distance < 2 && speed < 0.5)
+        if (t_dist < 2 && speed < 0.5)
         {
             Debug.Log("success");
             AddReward(2000);
