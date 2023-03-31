1ca52afe39fea8824f8d5990256360212fa82aee
wangning1996chris <wangning1996chris@gmail.com>
2023-03-31 17:48:06 +0800

    更新通信脚本，智能体随时可以获取ROS中的信息

--- Examples/MAPD/Script/Communication/RosSubscriber.cs
+++ Examples/MAPD/Script/Communication/RosSubscriber.cs
@@ -1,19 +1,33 @@
 using UnityEngine;
 using Unity.Robotics.ROSTCPConnector;
-using 
+using RosMessageTypes.Sensor;
+using RosMessageTypes.Geometry;
 
-RosMessageTypes.Sensor;
 
 public class RosSubscriber : MonoBehaviour
 {
+    private string LaserScanInfo;
 
     void Start()
     {
         ROSConnection.GetOrCreateInstance().Subscribe<LaserScanMsg>("scan", PrintLaserScan);
+        // ROSConnection.GetOrCreateInstance().Subscribe<PoseStampedMsg>("odom", PrintOdomScan);
     }
 
     void PrintLaserScan(LaserScanMsg LaserScan)
     {
-        Debug.Log(string.Join(",",LaserScan.ranges));
+        // Debug.Log(string.Join(",",LaserScan.ranges));
+        LaserScanInfo = string.Join(",",LaserScan.ranges);
+    }
+
+    void PrintOdomScan(PoseStampedMsg OdomScan)
+    {
+        Debug.Log(string.Join(",",OdomScan));
+    }
+
+    public string Update()
+    {
+        Debug.Log(LaserScanInfo);
+        return LaserScanInfo;
     }
 }
